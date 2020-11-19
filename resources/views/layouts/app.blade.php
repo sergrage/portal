acaa27d29159d651c6c7ad1755d3ed89f82da350
Сергей и Виктория Шматовские <elenergetica87@mail.ru>
2020-11-19 15:48:15 +0300

    change version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
         <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> - Технологический сайт ПАО 'ТГК-1' филиала 'Карельский'</p>
         <a href="#">Создатель сайта: С.А. Шматовский</a>
         <div class="float-right d-none d-sm-inline-block">
-            <b>Version</b> 1.0.0
+            <b>Version</b> 0.1.0
         </div>
     </footer>
 </div>
